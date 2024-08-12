a64fcfb1f136f5de639e21f0030dd6348925db9e
Matthias Opitz <m.opitz@ucl.ac.uk>
2024-08-12 16:21:13 +0200

    bumped version to trigger installation of new external function

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024071600;                    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024081200;                    // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051707;                    // Requires this Moodle version.
 $plugin->component = 'report_feedback_tracker';     // Full name of the plugin (used for diagnostics).
