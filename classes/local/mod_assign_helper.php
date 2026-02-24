16172c671f12604c00537b393ac5dd51f3119b98
Matthias Opitz <opitz@gmx.de>
2026-02-24 11:01:04 +0000

    CTP-4133: refactor module code into own classes
    - introducing module helper classes
    - refactored get_markingurl() into helper classes

---
+++ classes/local/mod_assign_helper.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace report_feedback_tracker\local;
+use moodle_url;
+
+/**
+ * The assignment module helper class.
+ *
+ * @package    report_feedback_tracker
+ * @copyright  2026 onwards UCL
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     m.opitz <m.opitz@ucl.ac.uk>
+ */
+class mod_assign_helper extends module_helper {
+    /**
+     * Return the URL to the assignment marking page
+     *
+     * @return mixed
+     */
+    public function get_markingurl() {
+        return new moodle_url('/mod/assign/view.php', ['id' => $this->module->id, 'action' => 'grading']);
+    }
+}
